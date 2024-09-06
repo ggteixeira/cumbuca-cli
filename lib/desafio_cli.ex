920442161a92469fe43c94f1a441388d3795bf76
Guilherme Teixeira <24235344+ggteixeira@users.noreply.github.com>
2024-09-06 13:18:49 -0300

    Makes the CLI work with parameters using pattern matching

--- lib/desafio_cli.ex
+++ lib/desafio_cli.ex
@@ -7,11 +7,13 @@ defmodule DesafioCli do
   A função main recebe os argumentos passados na linha de
   comando como lista de strings e executa a CLI.
   """
-  def main(_args) do
-    IO.puts(phrase())
+  def main(args) do
+    IO.puts(phrase(args))
   end
 
-  def phrase() do
-    "Hello, world!"
+  def phrase(args) do
+    [key, value] = args
+    IO.puts("Chave: #{key}")
+    IO.puts("Valor: #{value}")
   end
 end
