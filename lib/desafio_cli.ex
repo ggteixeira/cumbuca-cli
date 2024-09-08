c89273218f1486f26eeda3b41c50c695ea65d2a8
Guilherme Teixeira <24235344+ggteixeira@users.noreply.github.com>
2024-09-07 23:15:51 -0300

    WIP: makes the SET function kinda work. it works-ish!

--- lib/desafio_cli.ex
+++ lib/desafio_cli.ex
@@ -7,6 +7,7 @@ defmodule DesafioCli do
   A função main recebe os argumentos passados na linha de
   comando como lista de strings e executa a CLI.
   """
+
   def main(_args) do
     get_input()
   end
@@ -17,7 +18,16 @@ defmodule DesafioCli do
     get_input()
   end
 
+  def set(key, value, data) do
+    Map.put(data, key, value)
+  end
+
   def manage_commands(command) do
-    [cmd, key, value] = String.split(command, " ", parts: 3, trim: true)
+    [_cmd, key, value] = String.split(command, " ", parts: 3, trim: true)
+
+    data = Map.new()
+    # data = %{"one" => :one, "two" => :two}
+    new_data = set(key, value, data)
+    IO.inspect(new_data)
   end
 end
