c27483d6a0b07fd1252bbf316477d6a90096a69b
Guilherme Teixeira <24235344+ggteixeira@users.noreply.github.com>
2024-09-08 17:47:56 -0300

    WIP

--- lib/desafio_cli.ex
+++ lib/desafio_cli.ex
@@ -3,6 +3,35 @@ defmodule DesafioCli do
   Ponto de entrada para a CLI.
   """
 
+  def get_input do
+    # IO.gets("> ") |> manage_commands()
+    IO.gets("> ") |> manage_commands()
+
+    get_input()
+  end
+
+  def set(data, key, value) do
+    new_data = Map.put(data, key, value)
+    IO.puts("\nI'm data function")
+    IO.inspect(new_data)
+    new_data
+  end
+
+  def manage_commands(command) do
+    [_cmd, key, value] = String.split(command, " ", parts: 3, trim: true)
+
+    data = %{}
+
+    set(data, key, value)
+
+    # new_data = Map.put(data, :coisa, "coisada")
+    # IO.inspect(new_data)
+    #
+    # newer_data = Map.put(new_data, :outra_coisa, "outra coisa coisilson")
+    #
+    # IO.inspect(newer_data)
+  end
+
   @doc """
   A função main recebe os argumentos passados na linha de
   comando como lista de strings e executa a CLI.
@@ -10,24 +39,5 @@ defmodule DesafioCli do
 
   def main(_args) do
     get_input()
-  end
-
-  def get_input do
-    IO.gets("> ") |> manage_commands()
-
-    get_input()
-  end
-
-  def set(key, value, data) do
-    Map.put(data, key, value)
-  end
-
-  def manage_commands(command) do
-    [_cmd, key, value] = String.split(command, " ", parts: 3, trim: true)
-
-    data = Map.new()
-    # data = %{"one" => :one, "two" => :two}
-    new_data = set(key, value, data)
-    IO.inspect(new_data)
   end
 end
