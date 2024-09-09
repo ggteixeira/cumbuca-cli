84894cddbae28b5d3bb473f5b36020653aa932cc
Guilherme Teixeira <24235344+ggteixeira@users.noreply.github.com>
2024-09-09 12:41:46 -0300

    WIP: fixes append behavior that was adding :ok atoms to the list apparently

--- lib/desafio_cli.ex
+++ lib/desafio_cli.ex
@@ -3,15 +3,32 @@ defmodule DesafioCli do
   Ponto de entrada para a CLI.
   """
 
-  def get_input(lista) do
+  def get_input(current_list) do
     input = IO.gets("> ")
-    listb = [manage_commands(input) | lista]
-    IO.inspect(listb)
-    get_input(listb)
+
+    # updated_list = [manage_commands(input) | current_list]
+    # [manage_commands(input) | current_list]
+
+    # IO.puts("append:")
+    # IO.inspect(updated_list)
+
+    IO.inspect([manage_commands(input) | current_list])
+
+    get_input([manage_commands(input) | current_list])
+    # get_input(updated_list)
   end
 
   def set(data, key, value) do
     Map.put(data, key, value)
+  end
+
+  def begin(_data, _key) do
+    # Enum.find(data, fn map -> map[key] == key end)
+    IO.puts("Rodou o begin!")
+
+    # date = [%{"testa" => "1"}, %{"teste" => "2"}, %{"testi" => "3"}]
+    # gotten = Enum.find(date, fn map -> map end)
+    # IO.inspect(gotten)
   end
 
   def manage_commands(command) do
@@ -20,20 +37,18 @@ defmodule DesafioCli do
     data = %{}
 
     case cmd do
-      "SET" -> IO.puts("CAFE")
+      "SET" -> set(data, key, value)
+      "BEGIN" -> begin(data, key)
       _ -> IO.puts("Unknown command: #{cmd}")
     end
-
-    set(data, key, value)
   end
 
   @doc """
   A função main recebe os argumentos passados na linha de
   comando como lista de strings e executa a CLI.
   """
-
   def main(_args) do
-    lista = []
-    get_input(lista)
+    data_list = []
+    get_input(data_list)
   end
 end
